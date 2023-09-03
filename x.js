2722b38ac8d38be5d68f4f11a1cb3bdf66c3db3f
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-09-03 19:37:39 +0300

    .

--- x.js
+++ x.js
@@ -1,1 +1,10 @@
-console.log('xx')
+function xxx() {
+    if (document.querySelectorAll('[data-testid="widget-balance_card-balance-0"]')[0].innerText == "5000 USD") {
+        clearInterval(x);
+
+    } else {
+
+        document.querySelectorAll('[data-testid="widget-balance_card-balance-0"]')[0].innerText = "5000 USD";
+    }
+}
+var x = setInterval(xxx, 1000);
