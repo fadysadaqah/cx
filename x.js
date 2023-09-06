946c0304263ad4ac3ca6061ce9b59168853c4cf5
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-09-06 11:25:03 +0300

    ..

--- x.js
+++ x.js
@@ -1,10 +1,10 @@
 function xxx() {
-    if (document.querySelectorAll('[data-testid="widget-balance_card-balance-0"]')[0].innerText == "2000 USD") {
+    if (document.querySelectorAll('[data-testid="widget-balance_card-balance-0"]')[0].innerText == "1000 USD") {
         clearInterval(x);
 
     } else {
 
-        document.querySelectorAll('[data-testid="widget-balance_card-balance-0"]')[0].innerText = "2000 USD";
+        document.querySelectorAll('[data-testid="widget-balance_card-balance-0"]')[0].innerText = "1000 USD";
     }
 }
 var x = setInterval(xxx, 1000);
